8d822bdec1ba823796b474764912de6ff6e6a5e9
user <user@FG10>
2016-10-14 10:04:20 +0800

    hw3修改

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -3,19 +3,29 @@ package fcu.iecs.oop;
 import java.util.Scanner;
 public class StringComparator {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner StringComparator = new Scanner(System.in);
+		String str1,str2;
+		do{
 		System.out.print("Enter a string 1 :");
-		String str1 = StringComparator.next();
+		str1 = StringComparator.next();
 		System.out.print("Enter a string 2 :");
-		String str2 = StringComparator.next();
-		if(str1.equalsIgnoreCase(str2)){
+		str2 = StringComparator.next();	
+		if(str1.equalsIgnoreCase(str2))
+		{
 			System.out.println("The two strings are same.");
+			break;
 		}
-		else{
+		else
+		{
 			System.out.println("The two string are not same.");
-		}
-	}
+		
+			}
+		}while(str1==str2);
 
+	}
 }
+
+
